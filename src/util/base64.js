62a4be4fd9280ce2ac9658c8ed976bb842cb842a
r44j <raaj.ahab@gmail.com>
2021-11-16 10:56:29 +0000

    increases the time to wait before concluding autofill-daemon is not installed

--- src/util/base64.js
+++
@@ -1,2 +0,0 @@
-export const toAscii = input => Buffer.from(input, 'base64').toString('ascii')
-export const toBase64 = input => Buffer.from(input, 'ascii').toString('base64')
--- src/util/connect-daemon.js
+++ src/util/connect-daemon.js
@@ -1,10 +1,9 @@
-import { toBase64 } from './base64'
 import errors from './errors'
 
 export default key => new Promise((resolve, reject) => {
   const timeoutId = setTimeout(() => {
     reject(errors.DaemonNotInstalledError)
-  }, 200)
+  }, 3000)
   
   // We Base64 the key because we do not want a situation where special chars in s3 keys
   // would break the url
