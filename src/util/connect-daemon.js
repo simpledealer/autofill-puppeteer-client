8f8e8f49740de5242df2b5a32c69b01990664e53
Ernest Hayford <quenginedev@gmail.com>
2022-01-06 10:55:03 +0000

    readable timeout

--- src/util/connect-daemon.js
+++ src/util/connect-daemon.js
@@ -1,12 +1,12 @@
 import errors from './errors'
 
-const TIMEOUT = 10000 // Ten seconds
+const TIMEOUT = 10_000 // Ten seconds
 
 export default key => new Promise((resolve, reject) => {
   const timeoutId = setTimeout(() => {
     reject(errors.DaemonNotInstalledError)
   }, TIMEOUT)
-  
+
   // We Base64 the key because we do not want a situation where special chars in s3 keys
   // would break the url
   window.location.href = `simple-dealer://${key}`
