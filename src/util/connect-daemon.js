3ecc5e730aaac1d096f0dddcf769b953a910c21f
Ernest Hayford <quenginedev@gmail.com>
2022-05-13 14:06:06 +0100

    remove all console logs

--- src/util/connect-daemon.js
+++ src/util/connect-daemon.js
@@ -1,7 +1,5 @@
 export default key => {
   if (typeof window !== 'undefined') {
     window.location.href = `simple-dealer://${key}`
-  } else {
-    console.log('Cannot call daemon protocol handler. This is not a browser environment')
   }
 }
